3554396c320773400e75bdf38b16de9eab4608f4
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2019-04-17 19:15:44 +0200

    bilateral models

--- run.py
+++ run.py
@@ -66,6 +66,22 @@ test_transforms = transforms.Compose([
     normalize
 ])
 
+bilateral_train_transforms = transforms.Compose([
+    lambda x: np.array(cv2.bilateralFilter(np.array(x), 10, 100, 50)),
+    transforms.RandomResizedCrop(IMAGE_SIZE[0]),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    normalize
+])
+
+bilateral_test_transforms = transforms.Compose([
+    lambda x: np.array(cv2.bilateralFilter(np.array(x), 10, 100, 50)),
+    transforms.Resize(256),
+    transforms.CenterCrop(IMAGE_SIZE),
+    transforms.ToTensor(),
+    normalize
+])
+
 pair_transforms = transforms.Compose([
     transforms.CenterCrop(IMAGE_SIZE),
     transforms.ToTensor(),
@@ -84,7 +100,7 @@ highpass_transforms = transforms.Compose([
                         std=[0.225, 0.225, 0.225])
 ])
 
-def load_data(dataset_name, split):
+def load_data(dataset_name, split, train_transforms=train_transforms, test_transforms=test_transforms):
     dataset_path = os.path.join(config.rootPath, 'datasets', dataset_name)
 
     istrain = split == 'train'
@@ -116,6 +132,9 @@ original_val_dataset, original_val_loader = load_data('miniimagenet', 'val')
 
 stylized_train_dataset, stylized_train_loader = load_data('stylized-miniimagenet-1.0', 'train')
 stylized_val_dataset, stylized_val_loader = load_data('stylized-miniimagenet-1.0', 'val')
+
+bilateral_original_train_dataset, bilateral_original_train_loader = load_data('miniimagenet', 'train', train_transforms=bilateral_train_transforms, test_transforms=bilateral_test_transforms)
+bilateral_original_val_dataset, bilateral_original_val_loader = load_data('miniimagenet', 'val', train_transforms=bilateral_train_transforms, test_transforms=bilateral_test_transforms)
 
 for dataset, loader in [
     (original_train_dataset, original_train_loader),
@@ -164,11 +183,17 @@ supported_models = {
     # 'vgg19_autoencoder_highpass': create_vgg19_autoencoder,
     # 'vgg19_autoencoder_swap': create_vgg19_autoencoder,
     # 'vgg19_autoencoder_mix': create_vgg19_autoencoder,
-    'vgg19_variational_autoencoder_min': create_vgg19_variational_autoencoder,
-    'vgg19_variational_autoencoder_smin': create_vgg19_variational_autoencoder,
-    'vgg19_variational_autoencoder_highpass': create_vgg19_variational_autoencoder,
-    'vgg19_variational_autoencoder_swap': create_vgg19_variational_autoencoder,
-    'vgg19_variational_autoencoder_mix': create_vgg19_variational_autoencoder
+    # 'vgg19_variational_autoencoder_min': create_vgg19_variational_autoencoder,
+    # 'vgg19_variational_autoencoder_smin': create_vgg19_variational_autoencoder,
+    # 'vgg19_variational_autoencoder_highpass': create_vgg19_variational_autoencoder,
+    # 'vgg19_variational_autoencoder_swap': create_vgg19_variational_autoencoder,
+    # 'vgg19_variational_autoencoder_mix': create_vgg19_variational_autoencoder,
+    'vgg19_vanilla_tune_fc_bilateral': create_vgg19_vanilla_tune_fc,
+    'vgg19_bn_all_tune_fc_bilateral': create_vgg19_bn_all_tune_fc,
+    'vgg19_in_single_tune_all_bilateral': create_vgg19_in_single_tune_all,
+    'vgg19_in_affine_single_tune_all_bilateral': create_vgg19_in_affine_single_tune_all,
+    'resnet50_tune_fc_0.01_bilateral': create_resnet50_bn_tune_fc,
+    'resnet50_bin_tune_fc_bilateral': create_resnet50_bin_tune_fc
 }
 
 models = {k:v for (k,v) in supported_models.items() if k in (config.model if config.model is not None else supported_models)}
@@ -217,8 +242,8 @@ if config.train:
                 config.numberOfEpochs,
                 config.learningRate,
                 logger,
-                original_train_loader,
-                original_val_loader, config.device,
+                bilateral_original_train_loader if 'bilateral' in model_name else original_train_loader,
+                bilateral_original_val_loader if 'bilateral' in model_name else original_val_loader, config.device,
                 similarity_weight=similarity_weight if 'similarity' in model_name else None,
                 load_data=load_data
             )
@@ -236,5 +261,5 @@ if config.train:
 
 # Check Performance
 
-# perf(models, model_directory, dataset_names, config.device, load_data=load_data, only_exists=config.exists)
+perf(models, model_directory, dataset_names, config.device, load_data=load_data, only_exists=config.exists)
 
